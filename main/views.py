36bfe5b892c6968308d7c4b37a5816bd8956cc09
clay <ardellmarclay@gmail.com>
2026-09-09 17:13:22 +0700

    feat: (tutorial 02) new page, views, models, template

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Marclay Ardell",
+        "npm": "2506621024",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Passionate computer science student at Universitas Indonesia, with a keen interest in software development and problem-solving. Constantly learning new things and developing my skills."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Marclay Ardell",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
