ecd2b9c0f8beab815a965908270d68d2edab3bbc
clay <ardellmarclay@gmail.com>
2026-09-11 15:04:06 +0700

    feat: add django user

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Experience
 
-# Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'started_at', 'ended_at', 'is_ongoing')
+    list_filter = ('category',)
+    search_fields = ('title', 'description')
+    readonly_fields = ('started_at',)
